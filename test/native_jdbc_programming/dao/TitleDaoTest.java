6ac4ff0a8679151e92918a7e7eb5caec26f7f845
SurinJeon <surin0901@naver.com>
2021-03-12 18:55:24 +0900

    update file

--- test/native_jdbc_programming/dao/TitleDaoTest.java
+++ test/native_jdbc_programming/dao/TitleDaoTest.java
@@ -12,30 +12,26 @@ import native_jdbc_programming.dao.impl.TitleDaoImpl;
 import native_jdbc_programming.dto.Title;
 
 
-//test method 순서 정해주기
-
-@FixMethodOrder(MethodSorters.NAME_ASCENDING) // 알파벳 순으로 수행하겠다
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TitleDaoTest {
 
-	private static TitleDao dao = TitleDaoImpl.getInstance(); // TitleDao는 부모기 때문에 부모를 참조해서 자손들로 접근해야 더 코드를 쓰기 용이함 
+	private static TitleDao dao = TitleDaoImpl.getInstance(); 
 	
 	@After
 	public void tearDown() throws Exception {
 		System.out.println();
-	} // 각 테스트 끝나고 구분 위해 공백 찍는 용도로 긁어옴
+	} 
 	
 	
 	@Test
 	public void test04SelectTitleByAll() {
 		System.out.printf("%s() %n", "testSelectTitleByAll");
 		List<Title> titleList = dao.selectTitleByAll();
-		Assert.assertNotNull(titleList); // null 이 아니면 데이터가 있다는 뜻이므로 성공임
+		Assert.assertNotNull(titleList); 
 		
-		titleList.stream().forEach(System.out::println); // << 아래 for문과 같은 출력문을 찍는 문장임 
-/*		for(Title t : titleList) {
-			System.out.println(t);
-		}
-*/	}
+		titleList.stream().forEach(System.out::println); 
+
+	}
 
 	@Test
 	public void test05SelectTitleByNo() {
@@ -51,7 +47,7 @@ public class TitleDaoTest {
 	public void test01InsertTitle() {
 		System.out.printf("%s() %n", "testInsertTitle");
 		Title newTitle = new Title(6, "인턴");
-		int res = dao.insertTitle(newTitle); // 반영된 행이 하나면 그건 잘 된거임
+		int res = dao.insertTitle(newTitle);
 		Assert.assertEquals(1, res);
 		System.out.println(res);
 		System.out.println(dao.selectTitleByNo(newTitle));
@@ -61,7 +57,7 @@ public class TitleDaoTest {
 	public void test02UpdateTitle() {
 		System.out.printf("%s() %n", "testUpdateTitle");
 		Title newTitle = new Title(6, "계약직");
-		int res = dao.updateTitle(newTitle); // 반영된 행이 하나면 그건 잘 된거임
+		int res = dao.updateTitle(newTitle); 
 		Assert.assertEquals(1, res);
 		System.out.println(res);
 		System.out.println(dao.selectTitleByNo(newTitle));
